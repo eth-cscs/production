74cc4b6b9311814801f3f9a4f0de98a15c53e0da
Luca <lucamar@cscs.ch>
2016-11-29 17:26:16 +0100

    Updating production_builds-dom.sh to change groups ownerships and permissions

--- jenkins-builds/production_builds-dom.sh
+++ jenkins-builds/production_builds-dom.sh
@@ -31,18 +31,22 @@ starttime=$(date +%s)
 
 # loop over list 
 for build in $list; do
+
 # define name and version of the current build
  version=$(basename ${build#[aA-z-Z]*-} .eb)
  name=${build%-${version}.eb}
- echo -e "\n==============================================================="
+
+ echo -e "\n===============================================================\n"
 # build
+
 ### START workaround for Dom
  fullpath=$(eb --search $build | grep -v = | awk '{print $2}');
  sed "s/44_GA_2.2.7_g4a6c213-2.1/34_2.2.5_g8ce7a9a-2.1/" $fullpath > ./$build
 ### END workaround for Dom
+
 # use module footer and adjust ownership and permissions for selected builds
  if [[ ${name} =~ "CPMD" || ${name} =~ "VASP" ]]; then
-  echo -e "\n Creating a footer for ${name} modulefile to warn users not belonging to group ${name,,}\n"
+  echo -e "Creating a footer for ${name} modulefile to warn users not belonging to group ${name,,}\n"
   cat > ${EASYBUILD_TMPDIR}/${name}.footer<<EOF
 if { [lsearch [exec groups] "${name,,}"]==-1 && [module-info mode load] } {
  puts stderr "WARNING: Only users belonging to group ${name,,} with a valid ${name} license are allowed to access ${name} executables and library files"}
@@ -53,10 +57,12 @@ EOF
   echo -e "\n Changing group ownership and permissions for ${name} folders:\n - ${EASYBUILD_PREFIX}/software/${name}"
   chgrp ${name,,} -R ${EASYBUILD_PREFIX}/software/${name}
   chmod -R o-rwx ${EASYBUILD_PREFIX}/software/${name}/*
+# standard build without need to add a module footer or adjusting ownership and permissions
  else 
   echo -e "eb ./$build -r --modules-header=$APPS/UES/login/daint-${ARCH}.h"
   eb ./$build -r --modules-header=$APPS/UES/login/daint-${ARCH}.h
  fi
+
 # create default
  echo -e "\n Creating file ${EASYBUILD_PREFIX}/modules/all/${name}/.version to set ${version} as default for ${name}"
  cat > ${EASYBUILD_PREFIX}/modules/all/${name}/.version<<EOF 
