445c5c450edeecec35ef65636b8bdef8ab95cd81
Guilherme <gppezzi@users.noreply.github.com>
2016-12-10 12:03:38 +0100

    adding $ARCH to build prefix
    to enable concurrent builds of the gpu/mc software stacks

--- jenkins-builds/production_builds.sh
+++ jenkins-builds/production_builds.sh
@@ -26,6 +26,7 @@ export EASYBUILD_PREFIX=$APPS/UES/jenkins/$OS/$ARCH/easybuild
 export EB_CUSTOM_REPOSITORY=$PWD/easybuild
 module use $PWD/easybuild/module
 module load Easybuild
+export EASYBUILD_BUILDPATH=/dev/shm/jenscscs/easybuild/stage-$ARCH
 echo -e "\n Easybuild setup:"
 echo -e " - EASYBUILD_PREFIX=$EASYBUILD_PREFIX"
 echo -e " - EB_CUSTOM_REPOSITORY=$EB_CUSTOM_REPOSITORY"
