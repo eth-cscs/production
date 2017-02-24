f7920993eab7d235bf1e9efaa89c59e33ab80443
Victor Holanda Rusu <victor.holanda@cscs.ch>
2017-02-24 15:43:22 +0100

    Adding the supported applications folders

--- js/cscs.js
+++ js/cscs.js
@@ -2,9 +2,6 @@
  * @author Victor Holanda Rusu (CSCS)
  *
  */
-
-// var __cscsSiteNavBar = "";
-// var __cscsSiteContainter = "";
 var __cscsMarkDown = "";
 
 function cscs_read_file_contents(filename, callback)
@@ -28,8 +25,6 @@ function cscs_read_file_contents(filename, callback)
 }
 
 function cscs_setup_site_content(navbarfile, sidebarfile) {
-  // __cscsSiteNavBar = navbarfile;
-  // __cscsSiteSidebar = sidebarfile;
 
   cscs_read_file_contents(navbarfile, function __populate_site_content(argument) {
     document.getElementById("cscs-site-content").innerHTML = argument;
@@ -58,7 +53,6 @@ function cscs_setup_site_content(navbarfile, sidebarfile) {
   presenterMode.onclick = __cscs_show_in_presenter_mode;
 
   __cscs_email_protector();
-
 }
 
 function cscs_setup_markdown_page_content(markdownFile)
@@ -115,14 +109,6 @@ function __cscs_show_in_presenter_mode() {
 
   var slideshow = remark.create({sourceUrl: __cscsMarkDown});
 
-  // document.addEventListener("keydown", keyDownEscapePresenterMode, false);
-  // function keyDownEscapePresenterMode(e) {
-  //   var keyCode = e.keyCode;
-  //   if(keyCode == 27) {
-  //     __cscs_exit_presentation_mode();
-  //   }
-  // }
-
   // the click blocks, so forcing full page reload for the click
   var presenterMode = document.getElementById('start-cscs-presenter-mode');
   presenterMode.style.display = 'none';
@@ -140,7 +126,8 @@ function __cscs_show_in_presenter_mode() {
 
 
 function __cscs_email_protector() {
-  $("#cscs-email-protector").prepend('<a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%69%6E%66%6F%40%63%73%63%73%2E%63%68">Contact us</a>');
+  $("#cscs-email-protector").prepend('<a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%69%6E%66%6F%40%63%73%63%73%2E%63%68">Contact CSCS</a>');
+  $("#cscs-email-protector").prepend('<a href="&#109;&#097;&#105;&#108;&#116;&#111;:&#104;&#101;&#108;&#112;&#064;&#099;&#115;&#099;&#115;&#046;&#099;&#104;">Contact the User Support Team</a><br/>');
 }
 
 function __cscs_mouseover_link() {
@@ -193,44 +180,9 @@ function __cscs_create_toc() {
 
 // This function destroys the remark presentation and restores the CSCS website
 function __cscs_exit_presentation_mode() {
-  // if ($.browser.mozilla) {
-  //   if ( $('html').hasClass('remark-container') ) {
-  //       $('html').removeClass('remark-container');
-  //       if ( $('body').hasClass('remark-container') ) {
-  //           $('body').removeClass('remark-container');
-  //       }
-  //       if ( $('body').hasClass('remark-presenter-mode') ) {
-  //           $('body').removeClass('remark-presenter-mode');
-  //       }
-  //       if ($(".remark-notes-area").length > 0) {
-  //           $(".remark-notes-area").remove();
-  //       }
-  //       if ($(".remark-slides-area").length > 0) {
-  //           $(".remark-slides-area").remove();
-  //       }
-  //       if ($(".remark-preview-area").length > 0) {
-  //           $(".remark-preview-area").remove();
-  //       }
-  //       if ($(".remark-backdrop").length > 0) {
-  //           $(".remark-backdrop").remove();
-  //       }
-  //       if ($(".remark-help").length > 0) {
-  //           $(".remark-help").remove();
-  //       }
-  //       if ($(".remark-pause").length > 0) {
-  //           $(".remark-pause").remove();
-  //       }
-  //       document.getElementById("cscs-site-content").innerHTML = null;
-  //       cscs_setup_site_content(__cscsSiteNavBar, __cscsSiteSidebar);
-  //       cscs_setup_markdown_page_content(__cscsMarkDown);
-  //       __cscs_create_toc();
-  //       console.log('ciao');
-  //   }    
-  // } else {
     if(document.location.domain == null)
       window.location.assign(document.location.pathname);
     else
       window.location.assign(document.location = document.location.domain+document.location.pathname);
-  // }
 }
 
