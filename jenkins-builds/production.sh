249eaa99e98b6a2a6411abeacdc768eaf3497d83
Luca <lucamar@cscs.ch>
2017-06-21 17:28:27 +0200

    Updating script production.sh

--- jenkins-builds/production.sh
+++ jenkins-builds/production.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -l
 
-# New modulefiles/software will automatically be added to xalt list of modulefiles (reversemap) at the end of this script.
-# Hence one should not use it as CI.
-# The xalt list of modulefiles will be updated only by user jenkins. So this script should only be used by jenkins.
+# New modules will be added to xalt list (reversemap) at the end of this script, so one shouldn't use it as CI.
+# The xalt list will be updated only by user jenkins, therefore this script can only be used by user jenkins.
 
 scriptname=$(basename $0)
 
@@ -75,12 +74,10 @@ if [ -z "$ARCH" ] && [[ $system =~ "daint" ]]; then
     exit 1
 fi
 
-#
-# These are additional args to be used based on the system
-#
+# optional EasyBuild arguments
 eb_args=""
 
-# defines OS VERSION and ARCH parsing the selected production filename
+# prints PREFIX, ARCH and the list of EasyBuild files from the production list
 echo -e "\n - PREFIX FOLDER: '$PREFIX'"
 if [ -n "$ARCH" ]; then
     echo -e " - ARCH: '$ARCH' \n"
@@ -106,7 +103,7 @@ export EASYBUILD_PREFIX=$PREFIX/easybuild
 export EB_CUSTOM_REPOSITORY=$PWD/easybuild
 module use $PWD/easybuild/module
 module load Easybuild
-export EASYBUILD_BUILDPATH=/dev/shm/jenscscs/easybuild/stage-$ARCH
+export EASYBUILD_BUILDPATH=/dev/shm/$USER/easybuild/stage
 echo -e "\n Easybuild setup:"
 echo -e " - EASYBUILD_PREFIX=$EASYBUILD_PREFIX"
 echo -e " - EB_CUSTOM_REPOSITORY=$EB_CUSTOM_REPOSITORY"
@@ -119,61 +116,55 @@ eb --show-config
 echo -e "\n Builds started on $(date)"
 starttime=$(date +%s)
 
-# loop over list
-for ((i = 0; i < ${#eb_files[@]}; i++)); do
-    build=${eb_files[$i]}
+# cumulative exit status of EasyBuild commands in the loop
+status=0
+# loop over the list of EasyBuild files to build
+for((i=0; i<${#eb_files[@]}; i++)); do
 
     echo -e "\n===============================================================\n"
 
 # define name and version of the current build
-    name=`echo ${build} | cut -d'-' -f 1`
+    name=$(echo ${eb_files[$i]} | cut -d'-' -f 1)
 
-    #
-    # VASP and CPMD builds
-    #
-    if [[ $build == *"VASP"* || $build == *"CPMD"* ]]; then
-
-        #echo -e "Creating a footer for ${name} modulefile to warn users not belonging to group ${name,,}\n"
+# build VASP and CPMD
+    if [[ $name =~ "VASP" || $name =~ "CPMD" ]]; then
+# creating a footer for ${name} modulefile to warn users not belonging to group ${name,,}
         tmp_footer=""
-        if [ "Xdaint" == "X$hostName" ]; then
-           tmp_footer="--modules-footer=${EASYBUILD_TMPDIR}/${name}.footer"
-           cat > ${EASYBUILD_TMPDIR}/${name}.footer<<EOF
+        if [[ system =~ "daint" ]; then
+            tmp_footer="--modules-footer=${EASYBUILD_TMPDIR}/${name}.footer"
+            cat > ${EASYBUILD_TMPDIR}/${name}.footer<<EOF
 if { [lsearch [exec groups] "${name,,}"]==-1 && [module-info mode load] } {
  puts stderr "WARNING: Only users belonging to group ${name,,} with a valid ${name} license are allowed to access ${name} executables and library files"
 }
 EOF
         fi
-        echo -e "eb $build -r ${eb_args} ${tmp_footer}\n"
-        eb $build -r ${eb_args} ${tmp_footer}
+        echo -e "eb ${eb_files[$i]} -r ${eb_args} ${tmp_footer}\n"
+        status=$[status+$(eb ${eb_files[$i]} -r ${eb_args} ${tmp_footer})]
 
-        # change permissions for selected builds (note that $USER needs to be member of the group to use the command chgrp)
+# change permissions for selected builds (note that $USER needs to be member of the group to use the command chgrp)
         echo -e "\n Changing group ownership and permissions for ${name} folders:\n - ${EASYBUILD_PREFIX}/software/${name}"
         chgrp ${name,,} -R ${EASYBUILD_INSTALLPATH}/software/${name}
         chmod -R o-rwx ${EASYBUILD_INSTALLPATH}/software/${name}/*
 
-        #
-        # The other builds
-        #
+# build other software
     else
-        echo -e "eb $build -r ${eb_args}"
-        eb $build -r ${eb_args}
+        echo -e "eb ${eb_files[$i]} -r ${eb_args}"
+        status=$[status+$(eb ${eb_files[$i]} -r ${eb_args})]
     fi
-
 done
 
-if [ "Xdaint" == "X$hostName" ]; then
-    # update xalt table of modulefiles
+if [[ $system =~ "daint" ]]; then
+# update xalt table of modulefiles
     echo "loading PrgEnv-cray"
     module load PrgEnv-cray/6.0.3
     echo "module use craypat apps"
     module use /apps/daint/UES/6.0.UP02/craypat/easybuild/modules/all
-    # Removing Easybuild module before the reverseMapD operation
-    # because spider the mapping the programs to Easybuild
+# removing Easybuild module before the reverseMapD operation
     module unload Easybuild
     echo "running reverseMapD"
-    userid=`id -u`
-    # run only for jenscscs user
-    if [ "X$userid" == "X23395" ]; then
+    userid=$(id -u)
+# commands run by jenscscs user only
+    if [ $userid -eq 23395 ]; then
         module load Lmod
         export PATH=$EBROOTLMOD/lmod/7.1/libexec:$PATH  # !!! for spider !!!
         export XALTJENKINS=/apps/daint/UES/xalt/JENSCSCS
@@ -195,3 +186,5 @@ difftime=$(($endtime-$starttime))
  ((m=(${difftime}%3600)/60))
  ((s=${difftime}%60))
 echo -e "\n Builds ended on $(date) (elapsed time is $difftime s : ${h}h ${m}m ${s}s) \n"
+
+exit $[status+$?]
