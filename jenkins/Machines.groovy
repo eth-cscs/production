bb68f91780a7e92e7847ac5e220e0cfc81ce4a4e
Theofilos Manitaras <manitart@cscs.ch>
2017-11-15 14:53:19 +0100

    Source the .profile in certain machines

--- jenkins/Machines.groovy
+++ jenkins/Machines.groovy
@@ -3,36 +3,41 @@ def daint = [name: 'daint',
              toolkits: ['CrayCCE', 'CrayGNU', 'CrayIntel', 'CrayPGI'],
              toolkitVersions: ['17.08'],
              unusePath: '/apps/daint/UES/jenkins/6.0.UP04/ARCH/easybuild/modules/all',
-             scratchCommand: 'echo $SCRATCH']
+             scratchCommand: 'echo $SCRATCH',
+             sourceProfile: '']
+
 
 def dom = [name: 'dom',
            archs: ['gpu', 'mc'],
            toolkits: ['CrayCCE', 'CrayGNU', 'CrayIntel', 'CrayPGI'],
            toolkitVersions: ['17.08'],
            unusePath: '/apps/daint/UES/jenkins/6.0.UP04/ARCH/easybuild/modules/all',
-           scratchCommand: 'echo $SCRATCH']
+           scratchCommand: 'echo $SCRATCH',
+           sourceProfile: '']
 
 def kesch = [name: 'kesch',
              archs: [],
              toolkits: ['gmvolf', 'CrayCCE', 'GCC', 'GCCcore', 'gmvapich2', 'foss'],
              toolkitVersions: ['17.02'],
              unusePath: '/apps/escha/UES/generic/modulefiles:/apps/escha/UES/PrgEnv-gnu-17.02/modulefiles:/apps/escha/UES/PrgEnv-cray-17.06/modulefiles:/apps/escha/UES/experimental/modulefiles',
-             scratchCommand: 'echo /scratch/jenscscs']
-
+             scratchCommand: 'echo /scratch/jenscscs',
+             sourceProfile: 'source $HOME/.profile']
 
 def leone = [name: 'leone',
              archs: [],
              toolkits: ['GCC', 'GCCcore', 'gmvapich2', 'gmvolf', 'foss'],
              toolkitVersions: ['17.06'],
              unusePath: '/apps/leone/UES/PrgEnv-gnu-2016b', 
-             scratchCommand: 'echo /scratch/leone/jenscscs']
+             scratchCommand: 'echo /scratch/leone/jenscscs',
+             sourceProfile: 'source $HOME/.profile']
 
 def monch = [name: 'monch',
              archs: [],
              toolkits: ['GCC', 'GCCcore', 'gmvapich2', 'gmvolf', 'foss'],
              toolkitVersions: ['17.06'],
              unusePath: '/apps/monch/UES/jenkins/RH6.9-17.06/easybuild/modules/all/',
-             scratchCommand: 'source $HOME/.profile && echo $SCRATCH']
+             scratchCommand: 'source $HOME/.profile && echo $SCRATCH',
+             sourceProfile: 'source $HOME/.profile']
  
 
 return [daint, dom, kesch, leone, monch]
