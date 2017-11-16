6e1949c686d659cc5b9625383830c89180ef4ceb
bignamic <bignamini@cscs.ch>
2017-11-16 13:43:25 +0100

    Stop after build eb option added

--- jenkins-builds/production.sh
+++ jenkins-builds/production.sh
@@ -86,7 +86,7 @@ if [ -n "${force_list}" ]; then
 fi
 
 # optional EasyBuild arguments
-eb_args=""
+eb_args="--stop=build"
 
 # system name (excluding node number)
 if [[ "$HOSTNAME" =~ esch ]]; then
