aeb806d00cef7dad9e76fa699b74e7a6656758a7
victorusu <victorusu@users.noreply.github.com>
2019-02-18 20:15:32 +0100

    Update production.sh
    Updated the `export XALTPROD=/apps/daint/UES/xalt/production` path, but I think the issue is related to the permissions on `export XALTJENKINS=/apps/daint/UES/xalt/JENSCSCS`. They should be owned by the Jenkins' user in our systems.

--- jenkins-builds/production.sh
+++ jenkins-builds/production.sh
@@ -214,7 +214,7 @@ if [[ $system =~ "daint" && $update_xalt_table =~ "y" ]]; then
         module load Lmod/.7.8.2
         export PATH=$EBROOTLMOD/lmod/7.1/libexec:$PATH  # !!! for spider !!!
         export XALTJENKINS=/apps/daint/UES/xalt/JENSCSCS
-        export XALTPROD=/apps/daint/UES/xalt/git
+        export XALTPROD=/apps/daint/UES/xalt/production
         cd $XALTJENKINS/
         rm -rf $XALTJENKINS/reverseMapD
         ./cray_build_rmapT.sh .
