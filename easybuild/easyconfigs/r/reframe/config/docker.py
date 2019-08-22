5ca84f9d4ad1cad043691146c551f8ee3eea49c4
jgp <jgp@cscs.ch>
2019-08-22 10:47:12 +0200

    config

---
+++ easybuild/easyconfigs/r/reframe/config/docker.py
@@ -0,0 +1,132 @@
+class ReframeSettings:
+    reframe_module = 'reframe'
+    job_poll_intervals = [1, 2, 3]
+    job_submit_timeout = 60
+    checks_path = ['checks/']
+    checks_path_recurse = True
+    site_configuration = {
+        'systems': {
+
+            'docker': {
+                'descr': 'Docker',
+                'hostnames': ['0dfb1e216f80'],
+                'modules_system': 'tmod4',
+                'resourcesdir': '/apps/common/UES/reframe/resources',
+                'partitions': {
+
+                    'mc': {
+                        'scheduler': 'local+mpiexec',
+                        'environs': ['PrgEnv-gnu'],
+                        'descr': 'Docker TDS',
+                        'max_jobs': 5,
+                    },
+                }
+            },
+        },
+
+
+
+        'environments': {
+
+            '*': {
+                'builtin': {
+                    'type': 'ProgEnvironment',
+                    'cc':  'cc',
+                    'cxx': '',
+                    'ftn': '',
+                },
+
+                'PrgEnv-gnu': {
+                    'type': 'ProgEnvironment',
+                    'cc':  'mpicc',
+                    'cxx': 'mpicxx',
+                    'ftn': 'gfortran',
+                },
+
+                'builtin-gcc': {
+                    'type': 'ProgEnvironment',
+                    'cc':  'gcc',
+                    'cxx': 'g++',
+                    'ftn': 'gfortran',
+                }
+            }
+        },
+
+        'modes': {
+            '*': {
+                'maintenance': [
+                    '--exec-policy=async',
+                    '--strict',
+                    '--output=$APPS/UES/$USER/regression/maintenance',
+                    '--perflogdir=$APPS/UES/$USER/regression/maintenance/logs',
+                    '--stage=$SCRATCH/regression/maintenance/stage',
+                    '--reservation=maintenance',
+                    '--save-log-files',
+                    '--tag=maintenance',
+                    '--timestamp=%F_%H-%M-%S'
+                ],
+                'production': [
+                    '--exec-policy=async',
+                    '--strict',
+                    '--output=$APPS/UES/$USER/regression/production',
+                    '--perflogdir=$APPS/UES/$USER/regression/production/logs',
+                    '--stage=$SCRATCH/regression/production/stage',
+                    '--save-log-files',
+                    '--tag=production',
+                    '--timestamp=%F_%H-%M-%S'
+                ]
+            }
+        }
+    }
+
+    logging_config = {
+        'level': 'DEBUG',
+        'handlers': [
+            {
+                'type': 'file',
+                'name': 'reframe.log',
+                'level': 'DEBUG',
+                'format': '[%(asctime)s] %(levelname)s: '
+                          '%(check_info)s: %(message)s',
+                'append': False,
+            },
+
+            {
+                'type': 'stream',
+                'name': 'stdout',
+                'level': 'INFO',
+                'format': '%(message)s'
+            },
+            {
+                'type': 'file',
+                'name': 'reframe.out',
+                'level': 'INFO',
+                'format': '%(message)s',
+                'append': False,
+            }
+        ]
+    }
+
+    perf_logging_config = {
+        'level': 'DEBUG',
+        'handlers': [
+            {
+                'type': 'filelog',
+                'prefix': '%(check_system)s/%(check_partition)s',
+                'level': 'INFO',
+                'format': (
+                    '%(asctime)s|reframe %(version)s|'
+                    '%(check_info)s|jobid=%(check_jobid)s|'
+                    '%(check_perf_var)s=%(check_perf_value)s|'
+                    'ref=%(check_perf_ref)s '
+                    '(l=%(check_perf_lower_thres)s, '
+                    'u=%(check_perf_upper_thres)s)|'
+                    '%(check_perf_unit)s'
+                ),
+                'append': True
+            }
+        ]
+    }
+
+
+settings = ReframeSettings()
