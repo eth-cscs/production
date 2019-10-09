e5ec93690b9316ff5ba217dec25157d8011f40d4
Theofilos Manitaras <manitaras@cscs.ch>
2019-10-09 14:42:18 +0200

    Add cpus-per-task for srun commands

--- jenkins/Machines.groovy
+++ jenkins/Machines.groovy
@@ -1,5 +1,6 @@
 def daint = [name: 'daint',
              archs: ['gpu', 'mc'],
+             cpus: ['gpu': 12, 'mc': 36],
              buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
              unusePath: '/apps/daint/UES/jenkins/6.0.UP07/ARCH/easybuild/tools/modules/all:/apps/daint/UES/jenkins/6.0.UP07/ARCH/easybuild/modules/all',
              modulesProduction: '/apps/common/UES/jenkins/production/login',
@@ -8,22 +9,16 @@ def daint = [name: 'daint',
 
 def dom = [name: 'dom',
            archs: ['gpu', 'mc'],
+           cpus: ['gpu': 12, 'mc': 36],
            buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
            unusePath: '/apps/dom/UES/jenkins/7.0.UP01/ARCH/easybuild/tools/modules/all:/apps/dom/UES/jenkins/7.0.UP01/ARCH/easybuild/modules/all',
            modulesProduction: '',
            modulesUnuseProduction: '',
            prefixProduction: '$APPS/UES/jenkins/7.0.UP01/ARCH/easybuild']
 
-def fulen = [name: 'fulen',
-             archs: [],
-             buildPath: '/dev/shm/$USER/easybuild/build',
-             unusePath: '/apps/fulen/UES/modulefiles',
-             modulesProduction: '',
-             modulesUnuseProduction: '',
-             prefixProduction: '/apps/fulen/UES/jenkins/RH7.4/easybuild']
-
 def kesch = [name: 'kesch',
              archs: [],
+             cpus: 24,
              buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
              unusePath: '/apps/escha/UES/easybuild/modulefiles',
              modulesProduction: '',
