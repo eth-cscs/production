6f4342b017def565b4b8ed15c672a527e574408a
rafael <rafael.sarmiento@cscs.ch>
2019-10-11 11:08:18 +0200

    add hvd, tf, pyextensionsds with numpy-mkl

--- easybuild/easyconfigs/t/TensorFlow/configure-cscs-v2.0.0.sh
+++ easybuild/easyconfigs/t/TensorFlow/configure-cscs-v2.0.0.sh
@@ -10,8 +10,8 @@ echo "n" >> $CONF_IN # Do you wish to build TensorFlow with ROCm support? [y/N]:
 echo "y" >> $CONF_IN # Do you wish to build TensorFlow with CUDA support? [y/N]:
 echo "n" >> $CONF_IN # Do you wish to build TensorFlow with TensorRT support?
 echo "10.1" >> $CONF_IN # Please specify the CUDA SDK version you want to use
-echo "7.6.1" >> $CONF_IN # Please specify the cuDNN version you want to use.
-echo "2.4.7" >> $CONF_IN # Please specify the NCCL version you want to use.
+echo "7.6.4" >> $CONF_IN # Please specify the cuDNN version you want to use.
+echo "2.4.8" >> $CONF_IN # Please specify the NCCL version you want to use.
 echo "$EBROOTCUDA,$EBROOTCUDNN,$EBROOTNCCL" >> $CONF_IN # Please specify the comma-separated list of base paths to look for CUDA libraries and headers.
 echo "6.0" >> $CONF_IN # Please specify a list of comma-separated Cuda compute capabilities you want to build with.
 echo "n" >> $CONF_IN # Do you want to use clang as CUDA compiler? [y/N] n
