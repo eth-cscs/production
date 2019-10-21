286d58c25ff9ff0c1ecfc2f1cd35d54f90136f74
Theofilos Manitaras <manitaras@cscs.ch>
2019-10-21 14:14:07 +0200

    Remove module purge from production.sh
    * Also remove loading of additional cray modules.

--- jenkins-builds/production.sh
+++ jenkins-builds/production.sh
@@ -113,8 +113,6 @@ if [[ "$system" =~ "daint" || "$system" =~ "dom" ]]; then
         echo -e "\n No architecture defined. Please use the option -a,--arch to define the architecture \n"
         usage
     else
-        module purge
-        module load craype craype-network-aries modules perftools-base ugni
         module load daint-${ARCH}
         eb_args="${eb_args} --modules-header=${scriptdir%/*}/login/daint-${ARCH}.h --modules-footer=${scriptdir%/*}/login/daint.footer"
     fi
