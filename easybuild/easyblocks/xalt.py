50d4773a01a3ff85da16e1683a695ee94e2d2b4c
Victor Holanda Rusu <victor.holanda@cscs.ch>
2019-12-12 10:53:20 +0100

    Add support for XALT installation in production.sh
    Example case here is installation of XALT 2.7.24 on dom

---
+++ easybuild/easyblocks/xalt.py
@@ -0,0 +1,50 @@
+##
+# Copyright 2009-2019 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/easybuilders/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for building and installing XALT, implemented as an easyblockT
+
+@author: Victor Holanda (CSCS)
+"""
+
+import os
+import shutil
+
+from easybuild.easyblocks.generic.configuremake import ConfigureMake
+
+
+class EB_xalt(ConfigureMake):
+    """
+    xalt runtime and util packages
+    """
+
+    def configure_step(self, util=False):
+
+        self.original_installdir = self.installdir
+        self.basedir, _ = os.path.split(self.installdir)
+        self.basedir, _ = os.path.split(self.basedir)
+        self.installdir = self.basedir
+
+        super(EB_xalt, self).configure_step()
+        self.installdir = self.original_installdir
