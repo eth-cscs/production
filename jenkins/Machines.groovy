94f66ad87a96d6100ee3fc0ba5a1d8ed5a9db1b0
Matthias Kraushaar <kraushaar@cscs.ch>
2020-02-19 17:20:12 +0100

    Remove files already modified for RH7.6

--- jenkins/Machines.groovy
+++ jenkins/Machines.groovy
@@ -2,18 +2,16 @@ def daint = [name: 'daint',
              archs: ['gpu', 'mc'],
              cpus: ['gpu': 12, 'mc': 36],
              buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
-             unusePath: '/apps/daint/UES/jenkins/6.0.UP07/ARCH/easybuild/tools/modules/all:/apps/daint/UES/jenkins/6.0.UP07/ARCH/easybuild/modules/all',
+             unusePath: '/apps/daint/UES/jenkins/7.0.UP01/ARCH/easybuild/tools/modules/all:/apps/daint/UES/jenkins/7.0.UP01/ARCH/easybuild/modules/all',
              modulesProduction: '/apps/common/UES/jenkins/production/login',
-             modulesUnuseProduction: '/apps/daint/UES/easybuild/modulefiles',
-             prefixProduction: '$APPS/UES/jenkins/6.0.UP07/ARCH/easybuild']
+             prefixProduction: '$APPS/UES/jenkins/7.0.UP01/ARCH/easybuild']
 
 def dom = [name: 'dom',
            archs: ['gpu', 'mc'],
            cpus: ['gpu': 12, 'mc': 36],
            buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
            unusePath: '/apps/dom/UES/jenkins/7.0.UP01/ARCH/easybuild/tools/modules/all:/apps/dom/UES/jenkins/7.0.UP01/ARCH/easybuild/modules/all',
-           modulesProduction: '',
-           modulesUnuseProduction: '',
+           modulesProduction: '/apps/common/UES/jenkins/production/login',
            prefixProduction: '$APPS/UES/jenkins/7.0.UP01/ARCH/easybuild']
 
 def kesch = [name: 'kesch',
@@ -22,15 +20,6 @@ def kesch = [name: 'kesch',
              buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
              unusePath: '/apps/escha/UES/easybuild/modulefiles',
              modulesProduction: '',
-             modulesUnuseProduction: '',
              prefixProduction: '/apps/escha/UES/jenkins/RH7.5/generic/easybuild']
 
-def tsa = [name: 'tsa',
-           archs: [],
-           buildPath: '$XDG_RUNTIME_DIR/easybuild/build',
-           unusePath: '/apps/arolla/UES/modulefiles',
-           modulesProduction: '',
-           modulesUnuseProduction: '',
-           prefixProduction: '/apps/arolla/UES/jenkins/RH7.6/generic/easybuild']
-
-return [daint, dom, kesch, tsa]
+return [daint, dom, kesch]
