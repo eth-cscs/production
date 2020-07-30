9c0fa35acdd9ca3717c09a5e146543387aaaec7b
perettig <peretti@cscs.ch>
2020-07-30 14:37:38 +0200

    adding bulk generated 20.06 files from experimental branch

--- tools/update-pe-recursive.sh
+++ tools/update-pe-recursive.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #for TC in CrayGNU CrayIntel CrayCCE
-for TC in CrayGNU 
+for TC in CrayIntel CrayCCE 
 do
-    find -name *-$TC-2015.11* -exec python /apps/common/UES/tools/easybuild/new-tc.py --filename {} --toolchain $TC --version 2016.03 \;
+    find -name *-$TC-19.10* -exec python /apps/common/UES/tools/easybuild/new-tc.py --filename {} --toolchain $TC --version 20.06 \;
 done
