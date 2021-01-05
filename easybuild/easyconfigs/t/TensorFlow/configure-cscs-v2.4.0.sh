36af79d2eef7a3ba16d71fad4161d10dba63d048
rafael <rafael.sarmiento@cscs.ch>
2021-01-05 10:18:06 +0100

    some fixes

--- easybuild/easyconfigs/t/TensorFlow/configure-cscs-v2.4.0.sh
+++ easybuild/easyconfigs/t/TensorFlow/configure-cscs-v2.4.0.sh
@@ -13,20 +13,11 @@ echo "y" >> $CONF_IN
 # Do you wish to build TensorFlow with TensorRT support? [y/N]
 echo "n" >> $CONF_IN
 # Asking for detailed CUDA configuration...
-echo "11.0" >> $CONF_IN   # Please specify the CUDA SDK version you want to use. [Leave empty to default to CUDA 10]:
-echo "8.0.5" >> $CONF_IN  # Please specify the cuDNN version you want to use. [Leave empty to default to cuDNN 7]:
-echo "2.8.3" >> $CONF_IN  # Please specify the locally installed NCCL version you want to use. [Leave empty to use http://github.com/nvidia/nccl]:
+echo "$CUDATOOLKITVER" >> $CONF_IN   # Please specify the CUDA SDK version you want to use. [Leave empty to default to CUDA 10]:
+echo "$EBVERSIONCUDNN" >> $CONF_IN  # Please specify the cuDNN version you want to use. [Leave empty to default to cuDNN 7]:
+echo "$EBVERSIONNCCL" >> $CONF_IN  # Please specify the locally installed NCCL version you want to use. [Leave empty to use http://github.com/nvidia/nccl]:
 echo "$CRAY_CUDATOOLKIT_DIR,$EBROOTCUDNN,$EBROOTNCCL" >> $CONF_IN # Please specify the comma-separated list of base paths to look for CUDA libraries and headers.
 echo "6.0" >> $CONF_IN # Please specify a list of comma-separated Cuda compute capabilities you want to build with.
-# echo "n" >> $CONF_IN   # Do you wish to download a fresh release of clang? (Experimental) [y/N]
-# echo "n" >> $CONF_IN # Do you wish to build TensorFlow with XLA JIT support? [Y/n]:
-# echo "n" >> $CONF_IN # Do you wish to build TensorFlow with OpenCL SYCL support? [y/N]:
-# echo "n" >> $CONF_IN # Do you wish to build TensorFlow with ROCm support? [y/N]:
-# echo "y" >> $CONF_IN # Do you wish to build TensorFlow with CUDA support? [y/N]:
-# echo "n" >> $CONF_IN # Do you wish to build TensorFlow with TensorRT support?
-# echo "10.1" >> $CONF_IN # Please specify the CUDA SDK version you want to use
-# echo "7.6.4" >> $CONF_IN # Please specify the cuDNN version you want to use.
-# echo "2.4.8" >> $CONF_IN # Please specify the NCCL version you want to use.
 echo "n" >> $CONF_IN # Do you want to use clang as CUDA compiler? [y/N] n
 echo $GCC_PATH"/snos/bin/gcc" >> $CONF_IN # Please specify which gcc should be used by nvcc as the host compiler 
 echo "-march=native" >> $CONF_IN # Please specify optimization flags "--config=opt" :
