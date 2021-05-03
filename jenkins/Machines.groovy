fea37b0ded2e32ec61b5b51f2029e71bfb5309e5
Matthias Kraushaar <kraushaar@cscs.ch>
2021-05-03 15:55:45 +0200

    Change Prefix to MCH-PE20.08-UP01

--- jenkins/Machines.groovy
+++ jenkins/Machines.groovy
@@ -36,6 +36,6 @@ def mch_upgrade_rh79 = [name: 'mch_upgrade_rh79',
            unusePath: '/apps/arolla/UES/jenkins/RH7.7/MCH-PE20.08/generic/easybuild/modules/all',
            modulesProduction: '',
            modulesUnuseProduction: '',
-           prefixProduction: '/apps/arolla/UES/jenkins/RH7.9/MCH-PE20.08/generic/easybuild']
+           prefixProduction: '/apps/arolla/UES/jenkins/RH7.9/MCH-PE20.08-UP01/generic/easybuild']
 
 return [daint, dom, kesch, tsa, mch_upgrade_rh79]
