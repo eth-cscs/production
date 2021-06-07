3838d36303d3682e37a2afb5d72aea67bba16241
Luca <lucamar@cscs.ch>
2021-06-07 21:44:00 +0200

    Fix search string

--- jenkins/util.groovy
+++ jenkins/util.groovy
@@ -177,7 +177,7 @@ void createJiraIssue(String issuetype, String machine, String subject, String me
 */
 String[] searchJiraIssue(String subject, String machine, String pe, String project){
 
-   def search = jiraJqlSearch jql: "project = '$project' AND summary ~ '*$subject*' AND summary ~ '*$machine*' AND summary ~ '*$pe*'", fields: ['status'], maxResults: 1, site: 'JIRA_SITE', failOnError: false
+   def search = jiraJqlSearch jql: "project = '$project' AND summary ~ '$subject' AND summary ~ '$machine' AND summary ~ '*$pe*'", fields: ['status'], maxResults: 1, site: 'JIRA_SITE', failOnError: false
 
    if(search.successful && search.data.issues) {
        //  key and status of the Jira issues matched by the search 
